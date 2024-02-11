ef7e69a46e11cdd91f2e92fb02b6626c613ccda8
Diego Miranda <mirandadiego864@gmail.com>
2024-02-10 20:08:20 -0600

    mejora el funcionamiento

--- js/app.js
+++ js/app.js
@@ -5,20 +5,11 @@ let resultENDE = document.getElementById("result-en/de");
 
 function encrypt() {
   let mainText = document.getElementById("before-text").value;
-  let resultENDE = document.getElementById("result-en/de");
-  let showH1 = document.getElementById("show-h1");
-  let floatingHTML = document.getElementById("floating");
-  let personTextHTML = document.getElementById("person-text");
-  let sitPersonHTML = document.getElementById("sit-person");
-  showH1.innerText = "Texto encriptado:";
-  floatingHTML.style.borderTopRightRadius = "0px";
-  floatingHTML.style.justifyContent = "space-between";
-  resultENDE.style.width = "250px";
-  resultENDE.style.height = "400px";
-  personTextHTML.style.display = "flex";
-  personTextHTML.style.flexDirection = "column";
-  sitPersonHTML.style.display = "none";
-  btncopy.style.display = "block";
+  if (mainText.trim() === "") {
+    alert("Campo de texto vacio >:c");
+    return;
+  }
+
   let vowels = {
     a: "ai",
     e: "enter",
@@ -29,26 +20,17 @@ function encrypt() {
   let encryptedText = mainText.replace(/[aeiou]/gi, function (remplaceVowels) {
     return vowels[remplaceVowels.toLowerCase()];
   });
+  UpdateUI(encryptedText, 'Texto cifrado con éxito');
   resultENDE.innerText = encryptedText;
 }
 
 function decrypt() {
   let mainText = document.getElementById("before-text").value;
-  let resultENDE = document.getElementById("result-en/de");
-  let showH1 = document.getElementById("show-h1");
-  let floatingHTML = document.getElementById("floating");
-  let personTextHTML = document.getElementById("person-text");
-  let sitPersonHTML = document.getElementById("sit-person");
-  showH1.innerText = "Texto desencriptado:";
-  floatingHTML.style.borderTopRightRadius = "0px";
-  floatingHTML.style.display = "flex";
-  floatingHTML.style.justifyContent = "space-between";
-  resultENDE.style.width = "250px";
-  resultENDE.style.height = "400px";
-  personTextHTML.style.display = "flex";
-  personTextHTML.style.flexDirection = "column";
-  sitPersonHTML.style.display = "none";
-  btncopy.style.display = "block";
+  if (mainText.trim() === "") {
+    alert("Campo de texto vacio >:c");
+    return;
+    }
+
   let replaceVowels = {
     ai: "a",
     enter: "e",
@@ -56,14 +38,13 @@ function decrypt() {
     ober: "o",
     fat: "u",
   };
-  let decryptText = mainText.replace(
+  let decryptedText = mainText.replace(
     /(ai|enter|imes|ober|fat)/gi,
     function (remplaceEncrypt) {
       return replaceVowels[remplaceEncrypt.toLowerCase()];
     }
   );
-
-  resultENDE.innerText = decryptText;
+    UpdateUI(decryptedText, 'Texto descifrado con éxito' );
 }
 
 async function copy() {
@@ -71,10 +52,28 @@ async function copy() {
     await navigator.clipboard.writeText(resultENDE.value);
     window.alert("Copiado correctamente c:");
   } catch (err) {
-    console.log("error");
     window.alert("Error al copiar :c");
   }
 }
+
+function UpdateUI(text, message) {
+  
+  let showH1 = document.getElementById("show-h1");
+  let floatingHTML = document.getElementById("floating");
+  let personTextHTML = document.getElementById("person-text");
+  let sitPersonHTML = document.getElementById("sit-person");
+  showH1.innerText = message;
+  floatingHTML.style.borderTopRightRadius = "0px";
+  floatingHTML.style.justifyContent = "space-between";
+  resultENDE.style.width = "250px";
+  resultENDE.style.height = "400px";
+  personTextHTML.style.display = "flex";
+  personTextHTML.style.flexDirection = "column";
+  sitPersonHTML.style.display = "none";
+  btncopy.style.display = "block";
+  resultENDE.innerText = text;
+}
+
 
 btnencrypt.addEventListener("click", encrypt);
 btndecrypt.addEventListener("click", decrypt);
